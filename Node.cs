84689c147b76b7b0ee794726fd3555fdf4280de0
MohammedAlrefai <malrefai30@outlook.com>
2020-10-02 08:44:09 +0300

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+    public class Node {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+}
